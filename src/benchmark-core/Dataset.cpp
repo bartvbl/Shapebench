0ef2d1461448b8453eb8f2cf9ab1f16f5ef9e868
bartvbl <bartvblokl@gmail.com>
2023-10-09 17:48:51 +0200

    Added dataset handling

---
+++ src/benchmark-core/Dataset.cpp
@@ -0,0 +1,55 @@
+#include "Dataset.h"
+#include <fstream>
+#include <random>
+#include "json.hpp"
+
+void Dataset::load(std::filesystem::path &cacheFile) {
+    std::ifstream inputStream{cacheFile};
+    nlohmann::json cacheFileContents = nlohmann::json::parse(inputStream);
+
+    assert(cacheFileContents.contains("files"));
+    uint32_t fileCount = cacheFileContents.at("files").size();
+    entries.reserve(fileCount);
+
+    for(uint32_t i = 0; i < fileCount; i++) {
+        nlohmann::json jsonEntry = cacheFileContents.at("files").at(i);
+        bool isPointCloud = jsonEntry.at("isPointCloud");
+        // Exclude all point clouds
+        if(!isPointCloud) {
+            DatasetEntry entry;
+            entry.vertexCount = jsonEntry.at("vertexCount");
+            entry.id = jsonEntry.at("id");
+            entry.meshFile = std::string(jsonEntry.at("filePath"));
+            entries.push_back(entry);
+        }
+    }
+    std::sort(entries.begin(), entries.end());
+}
+
+std::vector<VertexInDataset> Dataset::sampleVertices(uint64_t randomSeed, uint32_t count) {
+    std::vector<uint32_t> sampleHistogram(entries.size());
+    std::vector<VertexInDataset> sampledEntries(count);
+
+    std::mt19937_64 engine(randomSeed);
+    std::uniform_int_distribution<uint32_t> distribution(0, entries.size());
+
+    for(uint32_t i = 0; i < count; i++) {
+        uint32_t chosenMeshIndex = distribution(engine);
+        sampleHistogram.at(chosenMeshIndex)++;
+    }
+    uint32_t nextIndex = 0;
+    for(uint32_t i = 0; i < entries.size(); i++) {
+        for(uint32_t j = 0; j < sampleHistogram.at(i); j++) {
+            sampledEntries.at(nextIndex).meshFile = entries.at(i).meshFile;
+            std::uniform_int_distribution<uint32_t> vertexIndexDistribution(0, entries.at(i).vertexCount);
+            sampledEntries.at(nextIndex).vertexIndex = vertexIndexDistribution(engine);
+            nextIndex++;
+        }
+    }
+    assert(nextIndex == count);
+    return sampledEntries;
+}
+
+bool DatasetEntry::operator<(DatasetEntry &other) {
+    return id < other.id;
+}
---
+++ src/benchmark-core/Dataset.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <filesystem>
+#include <vector>
+
+struct VertexInDataset {
+    std::filesystem::path meshFile;
+    uint32_t vertexIndex = 0;
+};
+
+struct DatasetEntry {
+    std::filesystem::path meshFile;
+    uint32_t vertexCount = 0;
+    uint32_t id = 0xFFFFFFFF;
+
+    bool operator<(DatasetEntry& other);
+};
+
+class Dataset {
+    std::vector<DatasetEntry> entries;
+public:
+    void load(std::filesystem::path &cacheFile);
+    std::vector<VertexInDataset> sampleVertices(uint64_t randomSeed, uint32_t count);
+};
