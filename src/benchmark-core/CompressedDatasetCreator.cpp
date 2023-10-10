cd1085d7f25f8c87680730f9b6edbdf43eecf22d
bartvbl <bartvblokl@gmail.com>
2023-10-10 18:43:20 +0200

    Also compute bounding sphere when creating compressed dataset

--- src/benchmark-core/CompressedDatasetCreator.cpp
+++ src/benchmark-core/CompressedDatasetCreator.cpp
@@ -12,6 +12,7 @@
 #include "shapeDescriptor/utilities/dump/meshDumper.h"
 #include <shapeDescriptor/utilities/free/mesh.h>
 #include <shapeDescriptor/utilities/free/pointCloud.h>
+#include <Seb.h>
 
 void Shapebench::computeCompressedDataSet(const std::filesystem::path &originalDatasetDirectory,
                                           const std::filesystem::path &compressedDatasetDirectory,
@@ -47,39 +48,62 @@ void Shapebench::computeCompressedDataSet(const std::filesystem::path &originalD
         datasetEntry["isPointCloud"] = isPointCloud;
         std::filesystem::path compressedMeshPath = compressedDatasetDirectory / filePath;
         compressedMeshPath.replace_extension(".cm");
+
+        std::vector<double> coordinate(3);
+        std::vector<Seb::Point<double>> vertices;
+
         try {
             if(isPointCloud) {
                 #pragma omp atomic
                 pointCloudCount++;
                 ShapeDescriptor::cpu::PointCloud cloud = ShapeDescriptor::utilities::loadPointCloud(datasetFiles.at(i));
-                ShapeDescriptor::utilities::writeCompressedGeometryFile(cloud, compressedMeshPath, true);
+                ShapeDescriptor::writeCompressedGeometryFile(cloud, compressedMeshPath, true);
                 datasetEntry["vertexCount"] = cloud.pointCount;
 
-                ShapeDescriptor::cpu::PointCloud readCloud = ShapeDescriptor::utilities::readPointCloudFromCompressedGeometryFile(compressedMeshPath);
-
+                // Integrity check
+                ShapeDescriptor::cpu::PointCloud readCloud = ShapeDescriptor::readPointCloudFromCompressedGeometryFile(compressedMeshPath);
                 if(ShapeDescriptor::comparePointCloud(cloud, readCloud)) {
                     std::cout << "\n!! POINT CLOUD HASH MISMATCH " + compressedMeshPath.string() + "\n" << std::flush;
                     #pragma omp atomic
                     hashMismatches++;
                 }
-                ShapeDescriptor::free::pointCloud(cloud);
                 ShapeDescriptor::free::pointCloud(readCloud);
+
+                vertices.reserve(cloud.pointCount);
+                for(uint32_t vertex = 0; vertex < cloud.pointCount; vertex++) {
+                    ShapeDescriptor::cpu::float3 point = cloud.vertices[vertex];
+                    coordinate.at(0) = point.x;
+                    coordinate.at(1) = point.y;
+                    coordinate.at(2) = point.z;
+                    vertices.emplace_back(3, coordinate.begin());
+                }
+
+                ShapeDescriptor::free::pointCloud(cloud);
             } else {
                 ShapeDescriptor::cpu::Mesh mesh = ShapeDescriptor::utilities::loadMesh(datasetFiles.at(i));
-                ShapeDescriptor::utilities::writeCompressedGeometryFile(mesh, compressedMeshPath, true);
+                ShapeDescriptor::writeCompressedGeometryFile(mesh, compressedMeshPath, true);
                 datasetEntry["vertexCount"] = mesh.vertexCount;
 
-
-                ShapeDescriptor::cpu::Mesh readMesh = ShapeDescriptor::utilities::readMeshFromCompressedGeometryFile(compressedMeshPath);
+                // Integrity check
+                ShapeDescriptor::cpu::Mesh readMesh = ShapeDescriptor::readMeshFromCompressedGeometryFile(compressedMeshPath);
                 if(!ShapeDescriptor::compareMesh(mesh, readMesh)) {
                     std::cout << "\n!! MESH HASH MISMATCH " + compressedMeshPath.string() + "\n" << std::flush;
                     #pragma omp atomic
                     hashMismatches++;
                     ShapeDescriptor::dump::mesh(readMesh, compressedMeshPath.replace_extension(".obj"));
                 }
+                ShapeDescriptor::free::mesh(readMesh);
+
+                vertices.reserve(mesh.vertexCount);
+                for(uint32_t vertex = 0; vertex < mesh.vertexCount; vertex++) {
+                    ShapeDescriptor::cpu::float3 point = mesh.vertices[vertex];
+                    coordinate.at(0) = point.x;
+                    coordinate.at(1) = point.y;
+                    coordinate.at(2) = point.z;
+                    vertices.emplace_back(3, coordinate.begin());
+                }
 
                 ShapeDescriptor::free::mesh(mesh);
-                ShapeDescriptor::free::mesh(readMesh);
             }
         } catch(std::runtime_error& e) {
             std::cout << "!! ERROR: FILE FAILED TO PARSE: " + filePath.string() + "\n   REASON: " + e.what() + "\n" << std::flush;
@@ -87,6 +111,14 @@ void Shapebench::computeCompressedDataSet(const std::filesystem::path &originalD
             datasetEntry["parseFailedReason"] = e.what();
             datasetEntry["vertexCount"] = -1;
         }
+
+        Seb::Smallest_enclosing_ball<double> ball(3, vertices);
+        double boundingRadius = ball.radius();
+        for(int j = 0; j < 3; j++) {
+            coordinate.at(j) = ball.center_begin()[j];
+        }
+        datasetEntry["boundingSphereCentre"] = {coordinate.at(0), coordinate.at(1), coordinate.at(2)};
+        datasetEntry["boundingSphereRadius"] = boundingRadius;
 
         #pragma omp critical
         {
