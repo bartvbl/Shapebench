2f1a0bce131f7bd5dc30207705f3a4411a2d2d44
Bart van Blokland <bartvblokl@gmail.com>
2023-10-22 20:17:35 +0200

    Implemented weighted hamming distance function

--- src/methods/QUICCIMethod.h
+++ src/methods/QUICCIMethod.h
@@ -8,7 +8,43 @@ namespace Shapebench {
         __device__ static __inline__ float computeDescriptorDistance(
                 const ShapeDescriptor::QUICCIDescriptor& descriptor,
                 const ShapeDescriptor::QUICCIDescriptor& otherDescriptor) {
-            return 0.5;
+
+            const uint32_t totalBitsInDescriptor = spinImageWidthPixels * spinImageWidthPixels;
+            const uint32_t chunkCount32Bit = totalBitsInDescriptor / 32;
+
+            uint32_t totalNeedleSetBitCount = 0;
+            uint32_t totalHaystackSetBitCount = 0;
+            uint32_t totalMissingSetPixelCount = 0;
+            uint32_t totalMissingUnsetPixelCount = 0;
+            for(unsigned int i = threadIdx.x; i < chunkCount32Bit; i+= blockDim.x) {
+                uint32_t needleChunk = descriptor.contents[i];
+                uint32_t haystackChunk = otherDescriptor.contents[i];
+
+                totalNeedleSetBitCount += __popc(needleChunk);
+                totalHaystackSetBitCount += __popc(haystackChunk);
+                totalMissingSetPixelCount += __popc((needleChunk ^ haystackChunk) & needleChunk);
+                totalMissingUnsetPixelCount += __popc((~needleChunk ^ ~haystackChunk) & ~needleChunk);
+            }
+
+            uint32_t combinedNeedleSetBitCount = ShapeDescriptor::warpAllReduceSum(totalNeedleSetBitCount);
+            uint32_t combinedHaystackSetBitCount = ShapeDescriptor::warpAllReduceSum(totalHaystackSetBitCount);
+
+            // Needle image is black, fall back to Hamming distance
+            if(combinedNeedleSetBitCount == 0) {
+                return float(combinedHaystackSetBitCount);
+            }
+
+            uint32_t combinedSetPixelCount = ShapeDescriptor::warpAllReduceSum(totalNeedleSetBitCount);
+            uint32_t combinedUnsetPixelCount = totalBitsInDescriptor - combinedSetPixelCount;
+
+            uint32_t combinedMissingSetCount = ShapeDescriptor::warpAllReduceSum(totalMissingSetPixelCount);
+            uint32_t combinedMissingUnsetCount = ShapeDescriptor::warpAllReduceSum(totalMissingUnsetPixelCount);
+
+            float missingSetBitPenalty = float(totalBitsInDescriptor) / float(max(combinedSetPixelCount, 1));
+            float missingUnsetBitPenalty = float(totalBitsInDescriptor) / float(max(combinedUnsetPixelCount, 1));
+
+            return float(combinedMissingSetCount) * missingSetBitPenalty +
+                   float(combinedMissingUnsetCount) * missingUnsetBitPenalty;
         }
 
         static bool usesMeshInput() {
