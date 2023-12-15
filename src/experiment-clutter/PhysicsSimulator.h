908c4aa88ef7b68120d17b7b4668fe1baf9f5ee8
bartvbl <bartvblokl@gmail.com>
2023-12-15 15:49:37 +0100

    Added WIP for computing convex hulls

--- src/experiment-clutter/PhysicsSimulator.h
+++ src/experiment-clutter/PhysicsSimulator.h
@@ -1,3 +1,14 @@
 #pragma once
 
+#include <shapeDescriptor/shapeDescriptor.h>
+#include "json.hpp"
+#include "benchmark-core/ComputedConfig.h"
+#include "benchmark-core/Dataset.h"
+
+struct ClutteredScene {
+    ShapeDescriptor::cpu::Mesh referenceMesh;
+    ShapeDescriptor::cpu::Mesh clutterGeometry;
+};
+
 void initPhysics();
+ClutteredScene createClutteredScene(const nlohmann::json& config, const ComputedConfig& computedConfig, const Dataset& dataset, uint64_t randomSeed);
