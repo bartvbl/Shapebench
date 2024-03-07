088f6cf46f69465b1a239b5f4b0a0b64ef61ebb0
bartvbl <bartvblokl@gmail.com>
2024-03-07 17:01:18 +0100

    Corrected direction in which distance is calculated

--- src/benchmarkCore/common-procedures/referenceIndexer.h
+++ src/benchmarkCore/common-procedures/referenceIndexer.h
@@ -6,11 +6,11 @@
 namespace ShapeBench {
     template<typename DescriptorMethod, typename DescriptorType>
     uint32_t computeImageIndex(const DescriptorType& cleanDescriptor, const DescriptorType& filteredDescriptor, ShapeDescriptor::cpu::array<DescriptorType> referenceSet) {
-        float sampleDescriptorDistance = DescriptorMethod::computeDescriptorDistance(cleanDescriptor, filteredDescriptor);
+        float sampleDescriptorDistance = DescriptorMethod::computeDescriptorDistance(filteredDescriptor, cleanDescriptor);
         //std::cout << "Distance to beat: " << sampleDescriptorDistance << std::endl;
         uint32_t filteredDescriptorRank = 0;
         for(uint32_t i = 0; i < referenceSet.length; i++) {
-            float referenceDescriptorDistance = DescriptorMethod::computeDescriptorDistance(cleanDescriptor, referenceSet[i]);
+            float referenceDescriptorDistance = DescriptorMethod::computeDescriptorDistance(filteredDescriptor, referenceSet[i]);
             if(referenceDescriptorDistance < sampleDescriptorDistance) {
                 filteredDescriptorRank++;
             }
