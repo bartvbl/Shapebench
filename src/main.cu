4f61721f96aebe499131c198693d38464f65d470
bartvbl <bartvblokl@gmail.com>
2024-03-25 16:16:15 +0100

    Changed priority

--- src/main.cu
+++ src/main.cu
@@ -84,9 +84,9 @@ int main(int argc, const char** argv) {
 
     uint64_t randomSeed = configuration.at("randomSeed");
     testMethod<ShapeBench::QUICCIMethod, ShapeDescriptor::QUICCIDescriptor>(configuration, configurationFile.value(), dataset, randomSeed);
+    testMethod<ShapeBench::RICIMethod, ShapeDescriptor::RICIDescriptor>(configuration, configurationFile.value(), dataset, randomSeed);
     testMethod<ShapeBench::SIMethod, ShapeDescriptor::SpinImageDescriptor>(configuration, configurationFile.value(), dataset, randomSeed);
     testMethod<ShapeBench::RoPSMethod, ShapeDescriptor::RoPSDescriptor>(configuration, configurationFile.value(), dataset, randomSeed);
-    testMethod<ShapeBench::RICIMethod, ShapeDescriptor::RICIDescriptor>(configuration, configurationFile.value(), dataset, randomSeed);
     //testMethod<ShapeBench::ShapeContextMethod, ShapeDescriptor::ShapeContextDescriptor>(configuration, configurationFile.value(), dataset, randomSeed);
     //testMethod<ShapeBench::FPFHMethod, ShapeDescriptor::FPFHDescriptor>(configuration, configurationFile.value(), dataset, randomSeed);
 }
