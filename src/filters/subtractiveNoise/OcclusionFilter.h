4591a878d4cf2f6cac95b298c40556836c3b1609
bartvbl (TURBONINJA) <bartvblokl@gmail.com>
2024-04-03 00:50:59 +0200

    Extracted the scene generator

--- src/filters/subtractiveNoise/OcclusionFilter.h
+++ src/filters/subtractiveNoise/OcclusionFilter.h
@@ -10,37 +10,9 @@
 #include "utils/gl/GeometryBuffer.h"
 #include "filters/FilteredMeshPair.h"
 #include "filters/Filter.h"
+#include "utils/filterUtils/OccludedSceneGenerator.h"
 
 namespace ShapeBench {
-    struct OcclusionRendererSettings {
-        float nearPlaneDistance = 0.001;
-        float farPlaneDistance = 100.0;
-        float fovy = 1.552;
-        float objectDistanceFromCamera = 20;
-    };
-
-    class OccludedSceneGenerator {
-        GLFWwindow* window = nullptr;
-        bool isDestroyed = false;
-	    bool isCreated = false;
-        Shader objectIDShader;
-        Shader fullscreenQuadShader;
-        uint32_t frameBufferID = 0;
-        uint32_t renderBufferID = 0;
-        uint32_t renderTextureID = 0;
-        GeometryBuffer screenQuadVAO;
-        uint32_t offscreenTextureWidth = 0;
-        uint32_t offscreenTextureHeight = 0;
-        std::mutex occlusionFilterLock;
-
-    public:
-        explicit OccludedSceneGenerator();
-        ~OccludedSceneGenerator();
-        ShapeBench::FilterOutput computeOccludedMesh(const nlohmann::json& config, ShapeBench::FilteredMeshPair &scene, uint64_t seed);
-        void init(uint32_t visibilityImageWidth, uint32_t visibilityImageHeight);
-        void destroy();
-    };
-
     class OcclusionFilter : public ShapeBench::Filter {
         OccludedSceneGenerator sceneGenerator;
 
