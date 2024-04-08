5f0ccac095ecd36431888488734ccecf20fdb9fb
bartvbl <bartvblokl@gmail.com>
2024-04-08 16:08:17 +0200

    Added support for support radius charts and a couple of filters

--- tools/charter/charter.py
+++ tools/charter/charter.py
@@ -2,12 +2,14 @@ import json
 import math
 import os
 import argparse
+import csv
 
 # Need to be installed on a fresh system
 import plotly.graph_objects as go
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.io as pio
+
 pio.kaleido.scope.mathjax = None
 
 
@@ -30,10 +32,11 @@ def getProcessingSettings(mode, fileContents):
         settings.xAxisTitle = "Normal vector rotation (degrees)"
         settings.yAxisTitle = sharedYAxisTitle
         settings.xAxisMin = 0
-        settings.xAxisMax = fileContents['configuration']['filterSettings']['normalVectorNoise']['maxAngleDeviationDegrees']
+        settings.xAxisMax = fileContents['configuration']['filterSettings']['normalVectorNoise'][
+            'maxAngleDeviationDegrees']
         settings.xTick = 5
         settings.reverse = False
-        settings.readValueX = lambda x : x["filterOutput"]["normal-noise-deviationAngle"]
+        settings.readValueX = lambda x: x["filterOutput"]["normal-noise-deviationAngle"]
         return settings
     elif experimentName == "subtractive-noise-only":
         settings.title = settings.methodName
@@ -59,8 +62,10 @@ def getProcessingSettings(mode, fileContents):
         settings.title = settings.methodName
         settings.xAxisTitle = "Support radius scale factor"
         settings.yAxisTitle = sharedYAxisTitle
-        settings.xAxisMin = 1 - fileContents['configuration']['filterSettings']['supportRadiusDeviation']['maxRadiusDeviation']
-        settings.xAxisMax = 1 + fileContents['configuration']['filterSettings']['supportRadiusDeviation']['maxRadiusDeviation']
+        settings.xAxisMin = 1 - fileContents['configuration']['filterSettings']['supportRadiusDeviation'][
+            'maxRadiusDeviation']
+        settings.xAxisMax = 1 + fileContents['configuration']['filterSettings']['supportRadiusDeviation'][
+            'maxRadiusDeviation']
         settings.xTick = 0.05
         settings.reverse = False
         settings.readValueX = lambda x: x["filterOutput"]["support-radius-scale-factor"]
@@ -75,21 +80,30 @@ def getProcessingSettings(mode, fileContents):
         settings.reverse = False
         settings.readValueX = lambda x: x["filterOutput"]["triangle-shift-average-edge-length"]
         return settings
+    elif experimentName == "gaussian-noise-only":
+        settings.title = settings.methodName
+        settings.xAxisTitle = "Standard Deviation"
+        settings.yAxisTitle = sharedYAxisTitle
+        settings.xAxisMin = 0
+        settings.xAxisMax = 0.01
+        settings.xTick = 0.001
+        settings.reverse = False
+        settings.readValueX = lambda x: x["filterOutput"]["gaussian-noise-max-deviation"]
+        return settings
     elif experimentName == "depth-camera-capture-only":
         settings.title = settings.methodName
         settings.xAxisTitle = "Object distance from camera"
         settings.yAxisTitle = sharedYAxisTitle
-        settings.xAxisMin = 0
-        settings.xAxisMax = 20
-        settings.xTick = 5
+        settings.xAxisMin = 2
+        settings.xAxisMax = 10
+        settings.xTick = 1
         settings.reverse = False
-        settings.readValueX = lambda x: (float(x["filterOutput"]["depth-camera-capture-initial-vertex-count"])
-                                         / float(x["filterOutput"]["depth-camera-capture-filtered-vertex-count"]))
+        settings.readValueX = lambda x: x["filterOutput"][
+            "depth-camera-capture-distance-from-camera"]  # (float(x["filterOutput"]["depth-camera-capture-initial-vertex-count"])
+        # / float(x["filterOutput"]["depth-camera-capture-filtered-vertex-count"]))
         return settings
     else:
         raise Exception("Failed to determine chart settings: Unknown experiment name: " + experimentName)
-
-
 
 
 def processSingleFile(jsonContent, settings):
@@ -128,9 +142,9 @@ def computeStackedHistogram(rawResults, config, settings):
             elif i == 1:
                 labels.append('1 - 10')
             else:
-                labels.append(str(int(10 ** (i-1)) + 1) + " - " + str(int(10 ** i)))
+                labels.append(str(int(10 ** (i - 1)) + 1) + " - " + str(int(10 ** i)))
 
-    xValues = [((float(x+1) * delta) + histogramMin) for x in range(settings.binCount)]
+    xValues = [((float(x + 1) * delta) + histogramMin) for x in range(settings.binCount)]
 
     removedCount = 0
     for rawResult in rawResults:
@@ -168,12 +182,84 @@ def computeStackedHistogram(rawResults, config, settings):
     return xValues, histogram, labels, counts
 
 
+def generateSupportRadiusChart(results_directory, output_directory):
+    csvFilePaths = [x.name for x in os.scandir(results_directory) if x.name.endswith(".txt")]
+    csvFilePaths.sort()
+
+    if len(csvFilePaths) == 0:
+        print("No json files were found in this directory. Aborting.")
+        return
+
+    for index, csvFileName in enumerate(csvFilePaths):
+        isLastFile = index + 1 == len(csvFilePaths)
+        csvFilePath = os.path.join(results_directory, csvFileName)
+        with open(csvFilePath) as csvFile:
+            print('Processing file:', csvFileName)
+            reader = csv.DictReader(csvFile)
+            sequenceDict = {}
+            for row in reader:
+                for key in row:
+                    key_stripped = key.strip()
+                    if key_stripped not in sequenceDict:
+                        sequenceDict[key_stripped] = []
+                    sequenceDict[key_stripped].append(float(row[key]))
+            xValues = sequenceDict["radius"]
+            yValues_Sequence1 = sequenceDict["Min mean"]
+            yValues_Sequence2 = sequenceDict["Mean"]
+            yValues_Sequence3 = sequenceDict["Max mean"]
+
+            countsFigure = go.Figure()
+
+            lineLocation = xValues[yValues_Sequence2.index(max(yValues_Sequence2))]
+            print(lineLocation)
+
+            methodName = csvFileName.split("_")[3]
+            yAxisRange = [0, max(yValues_Sequence3)]
+            useLog = False
+            if methodName == 'SI':
+                # The Spin image method uses Pearson Correlation
+                # Because as opposed to all other distance metrics, a higher number is better for Pearson, we computed 1 - pearsonDistance
+                # such that any distance would be between 0 and 2, where lower is better like all other methods
+                # We therefore need to do an inverse of that here to show correct distances in the chart
+                # (that make sense to the reader anyway)
+                yValues_Sequence1 = [1 - x for x in yValues_Sequence1]
+                yValues_Sequence2 = [1 - x for x in yValues_Sequence2]
+                yValues_Sequence3 = [1 - x for x in yValues_Sequence3]
+                yAxisRange = [-0.5, 0.5]
+            elif methodName == "RICI":
+                useLog = True
+                # Make line visible
+                yValues_Sequence1 = [max(x, 1) for x in yValues_Sequence1]
+                yAxisRange = [0, math.log10(max(yValues_Sequence3))]
+
+            yAxisTickMode = 'linear' if not useLog else 'log'
+
+            chartWidth = 285
+            if isLastFile:
+                #countsFigure.update_layout(legend=dict(y=0, orientation="h", yanchor="bottom", yref="container", xref="paper", xanchor="left"))
+                chartWidth = 350
+            else:
+                countsFigure.update_layout(showlegend=False)
+
+            pio.kaleido.scope.default_width = chartWidth
+            pio.kaleido.scope.default_height = 300
+
+            countsFigure.add_trace(go.Scatter(x=xValues, y=yValues_Sequence1, mode='lines', name="Min"))
+            countsFigure.add_trace(go.Scatter(x=xValues, y=yValues_Sequence2, mode='lines', name="Mean"))
+            countsFigure.add_trace(go.Scatter(x=xValues, y=yValues_Sequence3, mode='lines', name="Max"))
+            countsFigure.add_vline(x=lineLocation)
+            countsFigure.update_yaxes(range=yAxisRange, type=yAxisTickMode)
+            countsFigure.update_layout(xaxis_title="Radius", yaxis_title='Distance',
+                                       title_x=0.5, margin={'t': 2, 'l': 0, 'b': 0, 'r': 10}, width=chartWidth, height=270,
+                                       font=dict(size=18), xaxis=dict(tickmode='linear', dtick=0.5, range=(0, max(xValues))))
+
+            outputFile = os.path.join(output_directory, "support-radius-" + methodName + ".pdf")
+
+            pio.write_image(countsFigure, outputFile, engine="kaleido", validate=True)
+    print('Done.')
+
 
 def createChart(results_directory, output_directory, mode):
-    # Find all JSON files in results directory
-    jsonFilePaths = [x.name for x in os.scandir(results_directory) if x.name.endswith(".json")]
-    jsonFilePaths.sort()
-
     pio.templates[pio.templates.default].layout.colorway = [
         '#%02x%02x%02x' % (0, 128, 64),
         '#%02x%02x%02x' % (181, 137, 000),
@@ -188,6 +274,14 @@ def createChart(results_directory, output_directory, mode):
 
     if not os.path.exists(output_directory):
         os.makedirs(output_directory, exist_ok=True)
+
+    if mode == "support-radius":
+        generateSupportRadiusChart(results_directory, output_directory)
+        return
+
+    # Find all JSON files in results directory
+    jsonFilePaths = [x.name for x in os.scandir(results_directory) if x.name.endswith(".json")]
+    jsonFilePaths.sort()
 
     if len(jsonFilePaths) == 0:
         print("No json files were found in this directory. Aborting.")
@@ -208,15 +302,16 @@ def createChart(results_directory, output_directory, mode):
             lastSettings = settings
             dataSequence, rawResults = processSingleFile(jsonContents, settings)
 
-            stackedXValues, stackedYValues, stackedLabels, counts = computeStackedHistogram(rawResults, jsonContents["configuration"], settings)
+            stackedXValues, stackedYValues, stackedLabels, counts = computeStackedHistogram(rawResults, jsonContents[
+                "configuration"], settings)
             allCounts.append(counts)
             countLabels.append(settings.methodName)
             countXValues = stackedXValues
             stackFigure = go.Figure()
 
             for index, yValueStack in enumerate(stackedYValues):
-                stackFigure.add_trace(go.Scatter(x=stackedXValues, y=yValueStack, name=stackedLabels[index], stackgroup="main"))
-
+                stackFigure.add_trace(
+                    go.Scatter(x=stackedXValues, y=yValueStack, name=stackedLabels[index], stackgroup="main"))
 
             if jsonFilePath is not jsonFilePaths[-1]:
                 stackFigure.update_layout(showlegend=False)
@@ -231,16 +326,14 @@ def createChart(results_directory, output_directory, mode):
             stackFigure.update_yaxes(range=[0, 1])
             stackFigure.update_xaxes(range=[settings.xAxisMin, settings.xAxisMax])
 
+            stackFigure.update_layout(xaxis_title=settings.xAxisTitle, yaxis_title=settings.yAxisTitle, title_x=titleX,
+                                      margin={'t': 0, 'l': 0, 'b': 45, 'r': 0}, xaxis=dict(
+                    tickmode='linear',
+                    dtick=settings.xTick
+                ))
+            # stackFigure.show()
 
-            stackFigure.update_layout(xaxis_title=settings.xAxisTitle, yaxis_title=settings.yAxisTitle, title_x=titleX, margin={'t':0,'l':0,'b':45,'r':0}, xaxis = dict(
-        tickmode = 'linear',
-        dtick = settings.xTick
-    ))
-            #stackFigure.show()
-
-
-
-            outputFile =os.path.join(output_directory, settings.experimentName + "-" + settings.methodName + ".pdf")
+            outputFile = os.path.join(output_directory, settings.experimentName + "-" + settings.methodName + ".pdf")
             pio.write_image(stackFigure, outputFile, engine="kaleido", validate=True)
 
     print('Writing counts chart..')
@@ -251,14 +344,15 @@ def createChart(results_directory, output_directory, mode):
     countsFigure.update_yaxes(range=[0, math.log10(max([max(x) for x in allCounts]))], type="log")
     countsFigure.update_xaxes(range=[lastSettings.xAxisMin, lastSettings.xAxisMax])
     countsFigure.update_layout(xaxis_title=settings.xAxisTitle, yaxis_title='Sample Count',
-                              title_x=0.5, margin={'t': 2, 'l': 0, 'b': 0, 'r': 0}, width=285, height=270, font = dict(size = 10), xaxis = dict(
-        tickmode = 'linear',
-        dtick = settings.xTick * 2,
-            range = (lastSettings.xAxisMin, lastSettings.xAxisMax)
+                               title_x=0.5, margin={'t': 2, 'l': 0, 'b': 0, 'r': 0}, width=285, height=270,
+                               font=dict(size=10), xaxis=dict(
+            tickmode='linear',
+            dtick=settings.xTick * 2,
+            range=(lastSettings.xAxisMin, lastSettings.xAxisMax)
 
-
-    ))
-    countsFigure.update_layout(legend=dict(y=0, orientation="h", yanchor="bottom", yref="container", xref="paper", xanchor="left"))
+        ))
+    countsFigure.update_layout(
+        legend=dict(y=0, orientation="h", yanchor="bottom", yref="container", xref="paper", xanchor="left"))
     outputFile = os.path.join(output_directory, lastSettings.experimentName + "-counts.pdf")
     pio.kaleido.scope.default_width = 225
     pio.kaleido.scope.default_height = 300
@@ -269,10 +363,11 @@ def createChart(results_directory, output_directory, mode):
 
 def main():
     parser = argparse.ArgumentParser(description="Generates charts for the experiment results")
-    parser.add_argument("--results-directory", help="Directory containing JSON output files produced by ShapeBench", required=True)
+    parser.add_argument("--results-directory", help="Directory containing JSON output files produced by ShapeBench",
+                        required=True)
     parser.add_argument("--output-dir", help="Where to write the chart images", required=True)
     parser.add_argument("--mode", help="Specifies what the x-axis of the chart should represent",
-                        choices=["auto", "normal", "additive", "subtractive"],
+                        choices=["auto", "support-radius"],
                         default="auto", required=False)
     args = parser.parse_args()
 
@@ -287,7 +382,5 @@ def main():
     createChart(args.results_directory, args.output_dir, args.mode)
 
 
-
 if __name__ == "__main__":
     main()
-
