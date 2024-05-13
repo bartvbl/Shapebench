c9df8ee2fcdea37d2ab33947512863a146d48a3a
bartvbl <bartvblokl@gmail.com>
2024-05-13 12:03:35 +0200

    Remove possibility of exploding configuration file parsing

--- src/main.cu
+++ src/main.cu
@@ -148,12 +148,17 @@ int main(int argc, const char** argv) {
 nlohmann::json readConfiguration(std::filesystem::path filePath) {
     std::ifstream inputStream(filePath);
     nlohmann::json configuration = nlohmann::json::parse(inputStream);
+    nlohmann::json originalConfiguration = configuration;
     if(configuration.contains("includes")) {
         std::filesystem::path containingDirectory = filePath.parent_path();
         for(std::string dependencyPathString : configuration.at("includes")) {
             nlohmann::json subConfiguration = readConfiguration(containingDirectory / dependencyPathString);
             configuration.merge_patch(subConfiguration);
         }
+        // Delete includes key to ensure it does not get parsed twice
+        configuration.erase("includes");
     }
+    // We want the base file to override any values provided by any file it includes
+    configuration.merge_patch(originalConfiguration);
     return configuration;
 }
