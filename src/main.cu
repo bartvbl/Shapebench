9e32c80ec5b9255e695905e5727462606fbedc8f
bartvbl <bartvblokl@gmail.com>
2024-05-13 13:25:29 +0200

    Forgot to delete the copied includes

--- src/main.cu
+++ src/main.cu
@@ -157,6 +157,7 @@ nlohmann::json readConfiguration(std::filesystem::path filePath) {
         }
         // Delete includes key to ensure it does not get parsed twice
         configuration.erase("includes");
+        originalConfiguration.erase("includes");
     }
     // We want the base file to override any values provided by any file it includes
     configuration.merge_patch(originalConfiguration);
