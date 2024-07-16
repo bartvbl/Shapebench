359a0aabfa0804bb908d6026382588c3349b8e20
bartvbl <bartvblokl@gmail.com>
2024-07-16 15:17:05 +0200

    Updated terminal menu library

--- scripts/ubuntu_install_dependencies_except_cuda.sh
+++
@@ -1,2 +0,0 @@
-sudo apt install cmake python3 python3-pip libpcl-dev g++ gcc build-essential wget p7zip libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev
-pip3 install simple-term-menu xlwt xlrd numpy matplotlib pillow PyQt5 wcwidth
